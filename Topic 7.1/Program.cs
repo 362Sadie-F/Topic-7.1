b57a7305b449ffe8b2fc5fb9a7cb3806ab17199a
362Sadie-F <princessmae.fleming@gmail.com>
2026-03-09 12:42:15 -0400

    bank1

--- Topic 7.1/Program.cs
+++ Topic 7.1/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int pin = 45633;
+            int entry;
+
+            Console.WriteLine("WELCOME TO THE BANK");
+            Console.Write("ENTER YOUR PIN: ");
+            int.TryParse(Console.ReadLine(), out entry);
+            Console.WriteLine();
+            while (entry != pin)
+            {
+                Console.WriteLine("INCORRECT PIN. TRY AGAIN.");
+                Console.Write("ENTER YOUR PIN: ");
+                int.TryParse(Console.ReadLine(), out entry);
+                Console.WriteLine();
+            }
+            Console.WriteLine("PIN ACCEPTED. WELCOME IN.");
+
         }
     }
 }
