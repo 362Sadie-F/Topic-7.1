fee2677e8d9cd7b98aa36ba37128b89466aa7d5a
362Sadie-F <princessmae.fleming@gmail.com>
2026-03-10 13:13:49 -0400

    1

--- Topic 7.1/Program.cs
+++ Topic 7.1/Program.cs
@@ -8,31 +8,31 @@
             int count = 0;
             bool done = false;
 
-            //Console.WriteLine("WELCOME TO THE BANK");
-            //while (!done)
-            //{
-            //    Console.Write("ENTER YOUR PIN: ");
-            //    while (!int.TryParse(Console.ReadLine(), out pin));
-            //    Console.WriteLine("Please enter a valid number.");
-            //    tries++;
-            //    if (pin == entry)
-            //    {
-            //        Console.WriteLine("PIN ACCEPTED.");
-            //        done = true;
-            //    }
-            //    else if (tries == 5)
-            //    {
-            //        Console.WriteLine("Too many innocrect tries. Please try again later.");
-            //        done = true;
-            //    }
-            //}
-           
-            Console.WriteLine("Enter a message and I'll write it 5 times");
+            Console.WriteLine("WELCOME TO THE BANK");
+            while (!done)
+            {
+                Console.Write("ENTER YOUR PIN: ");
+                while (!int.TryParse(Console.ReadLine(), out pin)) ;
+                Console.WriteLine("Please enter a valid number.");
+                tries++;
+                if (pin == entry)
+                {
+                    Console.WriteLine("PIN ACCEPTED.");
+                    done = true;
+                }
+                else if (tries == 5)
+                {
+                    Console.WriteLine("Too many innocrect tries. Please try again later.");
+                    done = true;
+                }
+            }
+
+            Console.WriteLine("Enter a message and I'll write it 10 times");
             Console.Write("Message: ");
             string message = Console.ReadLine();
-            while (count < 5)
+            while (count < 10)
             {
-                Console.WriteLine((count +1) + ". " + message);
+                Console.WriteLine((count +10) + ". " + message);
                 count++;
             }
 
